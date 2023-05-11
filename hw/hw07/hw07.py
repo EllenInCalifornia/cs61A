6ccc3b8ad8803ea0d4cf4bc84b4c9b47e15b0617
Ellen Wang <claire19900125@gmail.com>
2023-05-11 10:47:12 +0800

    record an error of forgetting empty Link

--- hw/hw07/hw07.py
+++ hw/hw07/hw07.py
@@ -44,7 +44,11 @@ def deep_map_mut(func, lnk):
     >>> print(link1)
     <9 <16> 25 36>
     """
-    "*** YOUR CODE HERE ***"
+    if isinstance(lnk.first, Link):
+        deep_map_mut(func, lnk.first)
+    else:
+        lnk.first = func(lnk.first)
+        lnk.rest = deep_map_mut(func, lnk.rest)
 
 
 def two_list(vals, counts):
